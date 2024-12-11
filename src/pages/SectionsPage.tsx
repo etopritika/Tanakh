17eb94117c490199f1c554ef4af4ae68dfe43613
etopritika <dimaprytyka@gmail.com>
2024-12-11 12:41:59 +0200

    Updated sections page

--- src/pages/SectionsPage.tsx
+++ src/pages/SectionsPage.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
 const sections = [
-  { name: "Тора", path: "/sections/tora/books" },
-  { name: "Невиим", path: "/sections/neviim/books" },
-  { name: "Ктувим", path: "/sections/ketuvim/books" },
+  { name: "Тора", href: "/sections/tora/books" },
+  { name: "Невиим", href: "/sections/neviim/books" },
+  { name: "Ктувим", href: "/sections/ketuvim/books" },
 ];
 
 export default function SectionsPage() {
@@ -11,10 +11,10 @@ export default function SectionsPage() {
     <section className="py-6">
       <ul className="space-y-4">
         {sections.map((section) => (
-          <li key={section.path}>
+          <li key={section.href}>
             <Link
               className="inline-block bg-brown-dark text-white py-2 px-4 rounded-lg min-w-[100px] text-center"
-              to={section.path}
+              to={section.href}
             >
               {section.name}
             </Link>
