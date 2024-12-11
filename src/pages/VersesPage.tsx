6986fc6ce123e23a1ab1d50a66d11cdb2fa22cde
etopritika <dimaprytyka@gmail.com>
2024-12-12 00:37:41 +0200

    updated verses-page

--- src/pages/VersesPage.tsx
+++ src/pages/VersesPage.tsx
@@ -1,18 +1,52 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { fetchChapterData } from "@/lib/api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function VersesPage() {
+  const [verses, setVerses] = useState([]);
+  // console.log(verses);
   useEffect(() => {
     const loadChapter = async () => {
       const data = await fetchChapterData("tora", "beresheet", {
-        key: 1,
-        start: 0,
-        end: 32,
+        key: 7,
+        start: 162,
+        end: 185,
       });
-      console.log(data);
+      setVerses(data);
     };
 
     loadChapter();
   }, []);
-  return <span>VersesPage</span>;
+
+  return (
+    <section className="py-6 space-y-4">
+      <h1>{verses[0]?.chapter}</h1>
+      <ul className="space-y-4">
+        <li>
+          <Card>
+            <CardContent className="pt-6 space-y-2">
+              <p>{verses[0]?.verse}</p>
+              <p>{verses[0]?.verse_ivrit}</p>
+            </CardContent>
+          </Card>
+        </li>
+        <li>
+          <Card>
+            <CardContent className="pt-6 space-y-2">
+              <p>{verses[1]?.verse}</p>
+              <p>{verses[1]?.verse_ivrit}</p>
+            </CardContent>
+          </Card>
+        </li>
+        <li>
+          <Card>
+            <CardContent className="pt-6 space-y-2">
+              <p>{verses[2]?.verse}</p>
+              <p>{verses[2]?.verse_ivrit}</p>
+            </CardContent>
+          </Card>
+        </li>
+      </ul>
+    </section>
+  );
 }
