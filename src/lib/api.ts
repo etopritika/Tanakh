c00db76ffc3dbc194c4b5007f8473e6efbed0571
etopritika <dimaprytyka@gmail.com>
2024-12-12 19:44:27 +0200

    Updated fetch chapters logic

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,21 @@
 export const fetchChapterData = async (
   section: string,
   book: string,
-  chapter: { key: number; start: number; end: number }
+  chapterId: string
 ) => {
   try {
     const module = await import(`../data/${section}/obj-${book}.js`);
-
     const bookData = module.default;
+
+    const chaptersModule = await import(
+      `../lib/book-chapters/${section}/${book}-chapters.ts`
+    );
+    const chapters = chaptersModule.default;
+
+    const chapter = chapters.find(
+      (ch: { key: number; start: number; end: number; verses: number }) =>
+        String(ch.key) === chapterId
+    );
 
     const chapterData = bookData.slice(chapter.start, chapter.end + 1);
     return chapterData;
