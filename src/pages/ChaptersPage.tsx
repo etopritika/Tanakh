35fa8619f13cf9c029727b2ba70fe8f15745cbdd
etopritika <dimaprytyka@gmail.com>
2024-12-12 19:45:01 +0200

    updated chapters page

--- src/pages/ChaptersPage.tsx
+++ src/pages/ChaptersPage.tsx
@@ -1,16 +1,47 @@
-import { beresheetChapters } from "@/lib/book-chapters/tora/beresheet-chapters";
+import beresheetChapters from "@/lib/book-chapters/tora/beresheet-chapters";
+import bemidbarChapters from "@/lib/book-chapters/tora/bemidbar-chapters";
+import dvarimChapters from "@/lib/book-chapters/tora/dvarim-chapters";
+import schmotChapters from "@/lib/book-chapters/tora/schmot-chapters";
+import vaikraChapters from "@/lib/book-chapters/tora/vaikra-chapters";
 import { Link, useParams } from "react-router-dom";
 
+const chaptersMap: Record<
+  string,
+  { key: number; start: number; end: number; verses: number }[]
+> = {
+  beresheet: beresheetChapters,
+  bemidbar: bemidbarChapters,
+  dvarim: dvarimChapters,
+  schmot: schmotChapters,
+  vaikra: vaikraChapters,
+};
+
 export default function ChaptersPage() {
-  const { pageNumber, bookName, sectionName } = useParams<{
-    pageNumber: string;
+  const { chapterPage, bookName, sectionName } = useParams<{
+    chapterPage: string;
     bookName: string;
     sectionName: string;
   }>();
 
-  const chapters = beresheetChapters;
+  if (!bookName || !chaptersMap[bookName]) {
+    return (
+      <section className="py-6">
+        <div className="flex space-y-2 flex-col items-center">
+          <span className="text-danger">Главы отсутствуют</span>
+          <Link
+            className="inline-block bg-brown-dark text-white py-2 px-4 rounded-lg min-w-[150px] text-center"
+            to={`/sections/${sectionName}/books`}
+          >
+            Назад
+          </Link>
+        </div>
+      </section>
+    );
+  }
 
-  const page = parseInt(pageNumber || "1", 10);
+  const chapters = chaptersMap[bookName];
+
+  const page = parseInt(chapterPage || "1", 10);
 
   const chaptersPerPage = 10;
   const totalChapters = chapters.length;
