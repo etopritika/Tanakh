59c73246fdeb1ac67e328e6acce4fb96a801a259
etopritika <dimaprytyka@gmail.com>
2024-12-12 18:45:20 +0200

    Updated book chapters

---
+++ src/lib/book-chapters/tora/dvarim-chapters.ts
@@ -0,0 +1,44 @@
+export const dvarimChapters = [
+  {
+    key: 1,
+    start: 0,
+    end: 45,
+    verses: 46,
+  },
+  {
+    key: 2,
+    start: 46,
+    end: 82,
+    verses: 37,
+  },
+  {
+    key: 3,
+    start: 83,
+    end: 111,
+    verses: 29,
+  },
+  {
+    key: 4,
+    start: 112,
+    end: 160,
+    verses: 49,
+  },
+  {
+    key: 5,
+    start: 161,
+    end: 190,
+    verses: 30,
+  },
+  {
+    key: 6,
+    start: 191,
+    end: 215,
+    verses: 25,
+  },
+  {
+    key: 7,
+    start: 216,
+    end: 226,
+    verses: 11,
+  },
+];
