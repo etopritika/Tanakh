1786a0851e35ce9f6f152987adf42e1d8ac65062
etopritika <dimaprytyka@gmail.com>
2024-12-14 15:15:23 +0200

    updated pagination

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -72,11 +72,10 @@ const PaginationPrevious = ({
     to={to}
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 p-2.5 h-10 w-10", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
   </PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
@@ -90,10 +89,9 @@ const PaginationNext = ({
     to={to}
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 p-2.5 h-10 w-10", className)}
     {...props}
   >
-    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 );
