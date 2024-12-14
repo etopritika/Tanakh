6f52621fc53d9a19b5cd687ae55f60dd1f129fc1
etopritika <dimaprytyka@gmail.com>
2024-12-14 14:38:28 +0200

    Added verse list

---
+++ src/components/Verse-List.tsx
@@ -0,0 +1,22 @@
+import { Verse } from "@/lib/types";
+import { Card, CardContent } from "./ui/card";
+
+export default function VerseList({ verses }: { verses: Verse[] }) {
+  if (!verses.length) {
+    return <p>Список стихов пуст.</p>;
+  }
+  return (
+    <ul className="space-y-4">
+      {verses.map((verse) => (
+        <li key={verse.poemNumber}>
+          <Card className="bg-white">
+            <CardContent className="pt-6 space-y-2">
+              <p>{verse.verse}</p>
+              <p>{verse.verse_ivrit}</p>
+            </CardContent>
+          </Card>
+        </li>
+      ))}
+    </ul>
+  );
+}
