991a048304c161fd85a63d88769bb11f8f51bf79
etopritika <dimaprytyka@gmail.com>
2024-12-20 15:56:12 +0200

    Refactor: make scroll-up-button global

--- src/components/Scroll-up-button.tsx
+++ src/components/Scroll-up-button.tsx
@@ -2,39 +2,36 @@ import { useState, useEffect } from "react";
 import { ChevronsUp } from "lucide-react";
 import { Button } from "./ui/button";
 
-interface ScrollUpButtonProps {
-  scrollRef: React.RefObject<HTMLElement>;
-}
-
-export default function ScrollUpButton({ scrollRef }: ScrollUpButtonProps) {
+export default function ScrollUpButton() {
   const [isVisible, setIsVisible] = useState(false);
 
+  const toggleVisibility = () => {
+    setIsVisible(window.scrollY > 300);
+  };
+
+  const handleScrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   useEffect(() => {
-    const container = scrollRef.current;
-    if (!container) {
-      console.warn("Scroll container is not yet available.");
-      return;
-    }
-
-    const toggleVisibility = () => {
-      setIsVisible((container.scrollTop ?? 0) > 300);
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
     };
+  }, []);
 
-    container.addEventListener("scroll", toggleVisibility);
-
-    return () => container.removeEventListener("scroll", toggleVisibility);
-  }, [scrollRef]);
-
-  const handleScrollUp = () =>
-    scrollRef.current?.scrollTo({ top: 0, behavior: "smooth" });
-
-  return isVisible ? (
-    <Button
-      onClick={handleScrollUp}
-      aria-label="Scroll to top"
-      className="fixed bottom-2 right-2 z-50 rounded-lg border-none bg-brown-dark py-2 px-3 md:p-4"
-    >
-      <ChevronsUp className="text-white" />
-    </Button>
-  ) : null;
+  return (
+    isVisible && (
+      <Button
+        onClick={handleScrollUp}
+        aria-label="Scroll to top"
+        className="fixed bottom-4 right-4 z-50 rounded-full border-none bg-brown-dark md:p-6"
+      >
+        <ChevronsUp className="text-white" />
+      </Button>
+    )
+  );
 }
