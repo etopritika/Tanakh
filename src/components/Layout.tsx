d9772dc5edb28eb8c4a84aa90f0ad2f3af648ea2
etopritika <dimaprytyka@gmail.com>
2024-12-20 17:01:05 +0200

    updated global and container styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,19 +7,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const { pathname } = useLocation();
   const isSearchPage = pathname.includes("/search");
   return (
-    <>
+    <div className="h-screen container mx-auto px-4">
       <Header />
-      <div className="container mx-auto px-4">
-        <DesktopSidebar />
-        <main
-          className={`md:ml-80 ${
-            isSearchPage ? "mt-14 py-2 md:mt-0" : "mt-14"
-          }`}
-        >
-          {children}
-        </main>
-      </div>
+      <DesktopSidebar />
+      <main
+        className={`md:ml-80 h-full ${
+          isSearchPage ? "pt-14 py-2 md:pt-2" : "pt-14"
+        }`}
+      >
+        {children}
+      </main>
       <ScrollUpButton />
-    </>
+    </div>
   );
 }
