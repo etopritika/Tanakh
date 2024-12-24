de7c482454176b58e723849904da2ecf9dc65c7a
etopritika <dimaprytyka@gmail.com>
2024-12-24 14:57:19 +0200

    remodev coments

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,12 @@
-// import { useSearchStore } from "@/store/use-search-store";
 import SearchList from "@/components/SearchList";
 import SearchForm from "@/components/SearchForm";
 
 export default function SearchPage() {
-  // const { storeResults, error, isSearchComplete } = useSearchStore();
-
   return (
     <section className="py-6 space-y-4">
       <h1 className="text-xl font-bold">Поиск стихов</h1>
       <SearchForm />
 
-      {/* {error && <p className="text-danger">{error}</p>}
-      {storeResults.length === 0 && isSearchComplete && (
-        <div className="flex justify-center">
-          <span>Нет результатов.</span>
-        </div>
-      )} */}
       <SearchList />
     </section>
   );
