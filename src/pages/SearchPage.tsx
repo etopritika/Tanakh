eb84181c5b92bbd702b3e064a9af1ad3da41b219
etopritika <dimaprytyka@gmail.com>
2024-12-24 13:57:58 +0200

    updated search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -9,13 +9,12 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Link } from "react-router-dom";
-import { BookInfoMap, SearchFormData, searchSchema } from "@/lib/types";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { SearchFormData, searchSchema } from "@/lib/types";
 import { useDebouncedSearch } from "@/hooks/use-debounce-search";
 import { X } from "lucide-react";
 import { useSearchStore } from "@/store/use-search-store";
 import { useState } from "react";
+import SearchList from "@/components/SearchList";
 
 export default function SearchPage() {
   const { storeQuery, storeResults, error, clearSearch, setResults, setError } =
@@ -95,33 +94,7 @@ export default function SearchPage() {
           <span>Нет результатов.</span>
         </div>
       )}
-      <ul className="space-y-4">
-        {storeResults.map((verse) => {
-          const bookInfo = BookInfoMap[verse.id_book];
-          const to = `/${bookInfo.section}/${bookInfo.bookName}/${verse.id_chapter}#verse-${verse.poemNumber}`;
-
-          return (
-            <li
-              key={`${verse.id_book}-${verse.id_chapter}-${verse.poemNumber}`}
-            >
-              <Link to={to}>
-                <Card className="bg-white shadow-md">
-                  <CardHeader>
-                    <CardTitle className="text-base">{verse.chapter}</CardTitle>
-                  </CardHeader>
-                  <CardContent className="space-x-2 flex text-sm">
-                    <span className="font-bold">{verse.poemNumber}</span>
-                    <div className="space-y-2 w-full">
-                      <p>{verse.verse}</p>
-                      <p className="rtl text-right">{verse.verse_ivrit}</p>
-                    </div>
-                  </CardContent>
-                </Card>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <SearchList />
     </section>
   );
 }
