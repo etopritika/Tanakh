9b152372fa29b9999d43ff97e0524280db0b61b0
etopritika <dimaprytyka@gmail.com>
2024-12-29 10:49:28 +0200

    Added "back-link" to search page

---
+++ src/components/Header/Search-Back-Link.tsx
@@ -0,0 +1,20 @@
+import { ChevronLeft } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function SearchBackLink() {
+  const location = useLocation();
+
+  if (location.pathname !== "/search") {
+    return null;
+  }
+
+  return (
+    <Link
+      to={"#"}
+      onClick={() => window.history.back()}
+      className="px-4 py-2 text-text flex items-center"
+    >
+      <ChevronLeft /> Назад
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,18 +3,18 @@ import MobileSidebar from "../Sidebar/MobileSidebar";
 import { Search } from "lucide-react";
 import BackToChaptersLink from "./Back-To-Chapters-Link";
 import BackToBooksLink from "./Back-To-Books-Link";
+import SearchBackLink from "./Search-Back-Link";
 
 export default function Header() {
   const { pathname } = useLocation();
   const isSearchPage = pathname.includes("/search");
   return (
     <header
-      className={`fixed top-0 right-0 h-14 w-full mx-auto lg:pl-80 bg-background z-50 border-b flex justify-between px-4 py-1 ${
-        isSearchPage ? "lg:hidden" : ""
-      }`}
+      className={`fixed top-0 right-0 h-14 w-full mx-auto lg:pl-80 bg-background z-50 border-b flex justify-between px-4 py-1`}
     >
       <div className="flex items-center">
         <MobileSidebar />
+        <SearchBackLink />
         <BackToBooksLink />
         <BackToChaptersLink />
       </div>
