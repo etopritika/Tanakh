371ebcf7ec7c2539fbb6e5692572e971810aeb19
etopritika <dimaprytyka@gmail.com>
2024-12-31 14:28:28 +0200

    Removed comments from layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
 import DesktopSidebar from "./Sidebar/DesktopSidebar";
 import Header from "./Header";
-// import { useLocation } from "react-router-dom";
 import ScrollUpButton from "./Scroll-up-button";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // const { pathname } = useLocation();
-  // const isSearchPage = pathname.includes("/search");
   return (
     <div className="h-screen px-4">
       <Header />
