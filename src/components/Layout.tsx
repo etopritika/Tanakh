b811cc12fbd492d1a887e4a38615d4d3dc528c87
etopritika <dimaprytyka@gmail.com>
2025-01-28 13:09:11 +0200

    Added privateRoute and updated layout render

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,21 @@
+import { useLocation } from "react-router-dom";
+
 import Header from "./Header";
 import ScrollUpButton from "./Scroll-up-button";
 import DesktopSidebar from "./Sidebar/DesktopSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { pathname } = useLocation();
+  const isAuthPage = pathname === "/auth";
   return (
     <div className="h-screen px-4">
-      <Header />
-      <DesktopSidebar />
-      <main className="h-full pb-2 pt-14 lg:ml-80">{children}</main>
-      <ScrollUpButton />
+      {!isAuthPage && <Header />}
+      {!isAuthPage && <DesktopSidebar />}
+
+      <main className={`h-full ${!isAuthPage ? "pb-2 pt-14 lg:ml-80" : ""}`}>
+        {children}
+      </main>
+      {!isAuthPage && <ScrollUpButton />}
     </div>
   );
 }
