d21f9fe3f6f60a1fcc99db3cde33de51c5121c49
etopritika <dimaprytyka@gmail.com>
2025-01-31 16:30:35 +0200

    Added "name input" to register-form

--- src/store/use-search-store.ts
+++ src/store/use-search-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+
 import { Verse } from "@/lib/types";
 
 interface SearchStore {
---
+++ src/store/use-user-store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface UserState {
+  name: string | null;
+  setUserName: (name: string) => void;
+  clearUserName: () => void;
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      name: null,
+      setUserName: (name) => set({ name }),
+      clearUserName: () => set({ name: null }),
+    }),
+    { name: "user-storage" },
+  ),
+);
