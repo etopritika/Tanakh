360fd5eea3b7253325748bcc9f916da1337cf18d
etopritika <dimaprytyka@gmail.com>
2025-02-04 00:22:45 +0200

    Updated user-name components

--- src/components/UserName.tsx
+++ src/components/UserName.tsx
@@ -2,5 +2,5 @@ import { useUserStore } from "@/store/use-user-store";
 
 export default function UserName() {
   const { name } = useUserStore();
-  return <p>Рады вас видеть {name ? name : "Пользователь"}.</p>;
+  return <p>{name ? name : "Пользователь"}</p>;
 }
