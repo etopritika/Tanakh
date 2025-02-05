887b5658fd0d1068b7513e7285221ba107c4c994
etopritika <dimaprytyka@gmail.com>
2025-02-05 16:47:28 +0200

    Updated src

--- src/pages/BlueprintsPage.tsx
+++ src/pages/BlueprintsPage.tsx
@@ -3,7 +3,7 @@ export default function BlueprintsPage() {
     <section className="h-full py-6">
       <h1 className="sr-only">Blueprints Page</h1>
       <iframe
-        src="/sanctuary.pdf"
+        src={`${import.meta.env.BASE_URL}sanctuary.pdf`}
         className="h-full w-full border-none"
         title="Blueprint Viewer"
       />
