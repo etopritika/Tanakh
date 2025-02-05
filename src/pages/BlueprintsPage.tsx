3e3b4a566248e467478e4d111f585d22d1adb3d0
etopritika <dimaprytyka@gmail.com>
2025-02-05 12:56:50 +0200

    Added handle error in blueprints-page

--- src/pages/BlueprintsPage.tsx
+++ src/pages/BlueprintsPage.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 export default function BlueprintsPage() {
+  const [hasError, setHasError] = useState(false);
+
+  const handleIframeError = () => {
+    setHasError(true);
+  };
+
   return (
     <section className="h-full py-6">
       <h1 className="sr-only">Blueprints Page</h1>
-      <iframe
-        src="/blueprints/sanctuary.pdf"
-        className="h-full w-full border-none"
-        title="Blueprint Viewer"
-      />
+      {hasError ? (
+        <div className="flex h-full items-center justify-center text-center text-red-600">
+          <p>
+            Невозможно загрузить файл. Проверьте наличие файла или повторите
+            попытку.
+          </p>
+        </div>
+      ) : (
+        <iframe
+          src="/blueprints/sanctuary.pdf"
+          className="h-full w-full border-none"
+          title="Blueprint Viewer"
+          onError={handleIframeError}
+        />
+      )}
     </section>
   );
 }
