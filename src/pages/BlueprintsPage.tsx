12c06fc88b18f53c56301373ad57a51e13467c17
etopritika <dimaprytyka@gmail.com>
2025-02-05 16:37:09 +0200

    added iframe

--- src/pages/BlueprintsPage.tsx
+++ src/pages/BlueprintsPage.tsx
@@ -2,11 +2,11 @@ export default function BlueprintsPage() {
   return (
     <section className="h-full py-6">
       <h1 className="sr-only">Blueprints Page</h1>
-      {/* <iframe
+      <iframe
         src="/sanctuary.pdf"
         className="h-full w-full border-none"
         title="Blueprint Viewer"
-      /> */}
+      />
     </section>
   );
 }
