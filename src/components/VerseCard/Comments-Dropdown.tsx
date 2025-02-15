c9f61e32d6b9d38a2ab1fc2cfa83471b3a6bf215
etopritika <dimaprytyka@gmail.com>
2025-02-15 23:26:15 +0200

    Added Comments-Dropdown component

---
+++ src/components/VerseCard/Comments-Dropdown.tsx
@@ -0,0 +1,33 @@
+import { MessageSquareText } from "lucide-react";
+
+import CommentsPanel from "./Comments-Panel";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
+import { Verse } from "@/lib/types";
+
+export default function CommentsDropdown({ verse }: { verse: Verse }) {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="mt-4 w-full rounded bg-background px-3"
+    >
+      <AccordionItem value="comment">
+        <AccordionTrigger className="py-3 text-sm">
+          <div className="flex items-center space-x-2">
+            <span>Комментарии</span>
+            <MessageSquareText size={15} />
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className="py-3">
+          <CommentsPanel defaultVerse={verse} />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
