96010ddd5f3ed5a4b0bebe39f5fc755571db82da
etopritika <dimaprytyka@gmail.com>
2025-02-16 12:26:38 +0200

    Added firestore api

---
+++ src/components/VerseCard/actions.ts
@@ -0,0 +1,159 @@
+import { getAuth } from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  DocumentReference,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+
+import { app, db } from "@/lib/firebase";
+import { Comment } from "@/lib/types";
+
+export const fetchComments = async (bookName: string, verseId: string) => {
+  const auth = getAuth(app);
+  const currentUser = auth.currentUser?.uid;
+  if (!currentUser) return [];
+  const commentsRef = collection(
+    db,
+    "books",
+    bookName,
+    "verses",
+    verseId,
+    "comments",
+  );
+  try {
+    const q = query(
+      commentsRef,
+      where("uid", "==", currentUser),
+      orderBy("createdAt", "desc"),
+    );
+
+    const querySnapshot = await getDocs(q);
+    const loadedComments: Comment[] = [];
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data() as Comment;
+      loadedComments.push({
+        id: doc.id,
+        uid: data.uid,
+        text: data.text,
+        createdAt: data.createdAt,
+      });
+    });
+
+    return loadedComments;
+  } catch (error) {
+    console.error("Ошибка при загрузке комментариев: ", error);
+    return [];
+  }
+};
+
+export const addComment = async (
+  bookName: string,
+  verseId: string,
+  comment: string,
+): Promise<Comment> => {
+  const auth = getAuth(app);
+  const currentUser = auth.currentUser?.uid;
+
+  if (!currentUser) throw new Error("Пользователь не авторизован");
+
+  try {
+    const commentsRef = collection(
+      db,
+      "books",
+      bookName,
+      "verses",
+      verseId,
+      "comments",
+    );
+
+    const docRef: DocumentReference = await addDoc(commentsRef, {
+      uid: currentUser,
+      text: comment,
+      createdAt: new Date(),
+    });
+
+    return {
+      id: docRef.id,
+      uid: currentUser,
+      text: comment,
+      createdAt: new Date(),
+    };
+  } catch (error) {
+    console.error("Ошибка при добавлении комментария:", error);
+    throw error;
+  }
+};
+
+export const editComment = async (
+  bookName: string,
+  verseId: string,
+  commentId: string,
+  newText: string,
+): Promise<Comment> => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser?.uid;
+
+  if (!currentUser) throw new Error("Пользователь не авторизован.");
+
+  const updatedComment: Comment = {
+    id: commentId,
+    uid: currentUser,
+    text: newText,
+    createdAt: new Date(),
+  };
+
+  try {
+    const commentRef = doc(
+      db,
+      "books",
+      bookName,
+      "verses",
+      verseId,
+      "comments",
+      commentId,
+    );
+
+    await setDoc(commentRef, updatedComment, { merge: true });
+
+    return updatedComment;
+  } catch (error) {
+    console.error("Ошибка при редактировании комментария:", error);
+    throw error;
+  }
+};
+
+export const deleteComment = async (
+  bookName: string,
+  verseId: string,
+  commentId: string,
+): Promise<void> => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser?.uid;
+
+  if (!currentUser) throw new Error("Пользователь не авторизован.");
+
+  try {
+    const commentRef = doc(
+      db,
+      "books",
+      bookName,
+      "verses",
+      verseId,
+      "comments",
+      commentId,
+    );
+
+    await deleteDoc(commentRef);
+  } catch (error) {
+    console.error("Ошибка при удалении комментария:", error);
+    throw error;
+  }
+};
