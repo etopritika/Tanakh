0f1996465cd3437fc703ebb83e3ae4ab1cfe1090
etopritika <dimaprytyka@gmail.com>
2025-02-25 12:46:21 +0200

    Hide dropdown if no comments and removed "VerseContextMenu"

--- src/components/VerseCard/index.tsx
+++ src/components/VerseCard/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import CommentsDropdown from "./Comments-Dropdown";
 import VerseActionsDropdown from "./Verse-Actions-Dropdown";
-import VerseContextMenu from "./Verse-Context-Menu";
+// import VerseContextMenu from "./Verse-Context-Menu";
 import { Card, CardContent, CardFooter } from "../ui/card";
 
 import { Verse } from "@/lib/types";
@@ -14,10 +14,15 @@ export default function VerseCard({ verse }: { verse: Verse }) {
 
   const verseId = `verse-${verse.id_chapter}-${verse?.id_chapter_two || 1}-${verse.poemNumber}`;
   const verseMetadata = useFirestoreStore((state) => state.verses[verseId]);
+  const verseComments = Object.values(
+    useFirestoreStore((state) => state.comments[verseId]) ?? {},
+  );
 
   const docId = verseMetadata?.id;
 
   const highlightColor = verseMetadata?.highlightColor || "transparent";
+
+  const hasComments = Boolean(verse.comment) || verseComments.length > 0;
 
   const hash = window.location.hash;
 
@@ -48,46 +53,50 @@ export default function VerseCard({ verse }: { verse: Verse }) {
   };
 
   return (
-    <VerseContextMenu
-      verse={verse}
-      onCopy={handleCopy}
-      highlightColor={highlightColor}
-      docId={docId}
-    >
-      <li id={`verse-${verse.poemNumber}`}>
-        <Card
-          className={`bg-white shadow-md ${
-            isHighlighted ? "animate-pulse bg-muted text-white" : ""
-          }`}
+    // <VerseContextMenu
+    //   verse={verse}
+    //   onCopy={handleCopy}
+    //   highlightColor={highlightColor}
+    //   docId={docId}
+    // >
+    <li id={`verse-${verse.poemNumber}`}>
+      <Card
+        className={`bg-white shadow-md ${
+          isHighlighted ? "animate-pulse bg-muted text-white" : ""
+        }`}
+      >
+        <CardContent
+          className={`flex space-x-2 p-4 sm:space-x-3 sm:p-6 ${hasComments ? "pb-0 sm:pb-0" : ""}`}
         >
-          <CardContent className="flex space-x-2 p-4 pb-0 sm:space-x-3 sm:p-6 sm:pb-0">
-            <span className="font-bold">{verse.poemNumber}</span>
-            <div className="w-full space-y-2">
-              <p
-                style={{ backgroundColor: isCopied ? "" : highlightColor }}
-                className={`rounded ${isCopied ? "animate-pulse bg-muted text-white" : ""}`}
-              >
-                {verse.verse}
-              </p>
-              <p
-                style={{ backgroundColor: isCopied ? "" : highlightColor }}
-                className={`rtl rounded text-right ${isCopied ? "animate-pulse bg-muted text-white" : ""}`}
-              >
-                {verse.verse_ivrit}
-              </p>
-            </div>
-            <VerseActionsDropdown
-              verse={verse}
-              onCopy={handleCopy}
-              highlightColor={highlightColor}
-              docId={docId}
-            />
-          </CardContent>
+          <span className="font-bold">{verse.poemNumber}</span>
+          <div className="w-full space-y-2">
+            <p
+              style={{ backgroundColor: isCopied ? "" : highlightColor }}
+              className={`rounded ${isCopied ? "animate-pulse bg-muted text-white" : ""}`}
+            >
+              {verse.verse}
+            </p>
+            <p
+              style={{ backgroundColor: isCopied ? "" : highlightColor }}
+              className={`rtl rounded text-right ${isCopied ? "animate-pulse bg-muted text-white" : ""}`}
+            >
+              {verse.verse_ivrit}
+            </p>
+          </div>
+          <VerseActionsDropdown
+            verse={verse}
+            onCopy={handleCopy}
+            highlightColor={highlightColor}
+            docId={docId}
+          />
+        </CardContent>
+        {hasComments && (
           <CardFooter className="p-4 sm:p-6">
             <CommentsDropdown verse={verse} />
           </CardFooter>
-        </Card>
-      </li>
-    </VerseContextMenu>
+        )}
+      </Card>
+    </li>
+    // </VerseContextMenu>
   );
 }
