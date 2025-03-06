6223efb42eef416637c6bbbed398caa859654632
etopritika <dimaprytyka@gmail.com>
2025-03-06 10:33:49 +0200

    Updated redirect logic

--- src/components/VerseCard/Redirect-Button.tsx
+++ src/components/VerseCard/Redirect-Button.tsx
@@ -1,19 +1,38 @@
-import { Link } from "lucide-react";
+import { Link } from "react-router";
 
-import { Button } from "../ui/button";
+import { bookNameMap, SectionName, sectionNameMap } from "@/lib/types";
 
 export default function RedirectButton({
   redirectLink,
 }: {
   redirectLink: string;
 }) {
+  let section = "";
+  let book = "";
+  let chapter = "";
+  let verse = "";
+  let isValidFormat = false;
+
+  const url = new URL(redirectLink);
+  const parts = url.pathname.split("/");
+
+  if (parts[1] === "books" && url.hash.startsWith("#verse-")) {
+    const sectionKey = parts[2] as SectionName;
+    section = sectionNameMap[sectionKey] ?? parts[2];
+    book = bookNameMap[parts[3]] ?? parts[3];
+    chapter = parts[4];
+    verse = url.hash.split("-")[1];
+    isValidFormat = true;
+  }
+
   return (
-    <Button
-      variant="outline"
-      className="bg-white p-2"
-      onClick={() => window.open(redirectLink, "_blank", "noopener,noreferrer")}
+    <Link
+      to={redirectLink}
+      className="not-italic text-blue-600 underline hover:text-blue-800"
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      <Link size={20} />
-    </Button>
+      {isValidFormat ? `${section} ${book} ${chapter}:${verse}` : redirectLink}
+    </Link>
   );
 }
