f8d5fac02afc18ab3d7ca96b524e9a5cdacd74f6
etopritika <dimaprytyka@gmail.com>
2025-03-09 20:21:25 +0200

    Refactor: removed hover from button

--- src/components/Scroll-up-button.tsx
+++ src/components/Scroll-up-button.tsx
@@ -29,7 +29,7 @@ export default function ScrollUpButton() {
       <Button
         onClick={handleScrollUp}
         aria-label="Scroll to top"
-        className="fixed bottom-14 right-4 z-40 rounded-full border-none bg-brown-dark transition-colors hover:bg-brown-light md:p-6"
+        className="fixed bottom-14 right-4 z-40 rounded-full border-none bg-brown-dark md:p-6"
       >
         <ChevronsUp className="text-white" />
       </Button>
