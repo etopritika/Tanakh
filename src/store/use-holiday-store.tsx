8c903ba5e74561607c7e287d49cdbeb093cdc1fd
etopritika <dimaprytyka@gmail.com>
2025-03-18 16:30:41 +0200

    Added holiday store

---
+++ src/store/use-holiday-store.tsx
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+
+import { HolidayItem } from "@/lib/types";
+
+interface HolidayStore {
+  holidays: Record<number, Record<string, HolidayItem[]>>;
+  holidaysByJewishDate: Record<string, HolidayItem[]>;
+
+  selectedHoliday: HolidayItem[]; // Вибране свято
+
+  setHolidays: (
+    year: number,
+    holidaysByDate: Record<string, HolidayItem[]>,
+    holidaysByJewishDate: Record<string, HolidayItem[]>,
+  ) => void;
+
+  setSelectedHoliday: (holidays: HolidayItem[]) => void;
+}
+
+export const useHolidayStore = create<HolidayStore>((set) => ({
+  holidays: {},
+  holidaysByJewishDate: {},
+
+  selectedHoliday: [],
+
+  setHolidays: (year, holidaysByDate, holidaysByJewishDate) =>
+    set((state) => ({
+      holidays: {
+        ...state.holidays,
+        [year]: holidaysByDate,
+      },
+      holidaysByJewishDate: {
+        ...state.holidaysByJewishDate,
+        ...holidaysByJewishDate,
+      },
+    })),
+
+  setSelectedHoliday: (holidays) => set({ selectedHoliday: holidays }),
+}));
