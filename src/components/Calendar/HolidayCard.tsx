aa504eac7ba1f625e591bcd35dc57496db1747a3
etopritika <dimaprytyka@gmail.com>
2025-03-22 17:58:27 +0200

    Added links from calendar to book

--- src/components/Calendar/HolidayCard.tsx
+++ src/components/Calendar/HolidayCard.tsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+
 import { translateHolidayTitle } from "./utils/calendar-utils";
+import { parashatLinks } from "./utils/parashatLinks";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { useHolidayStore } from "@/store/use-holiday-store";
@@ -13,22 +16,40 @@ export default function HolidayCard() {
   return (
     <Card className="w-full max-w-lg bg-white">
       <CardContent className="space-y-3 pt-6">
-        {selectedHoliday.map((holiday) => (
-          <div
-            key={holiday.title}
-            className="border-b pb-2 last:border-b-0 last:pb-0"
-          >
-            <p className="text-lg font-bold">
-              {translateHolidayTitle(holiday.title)}
-            </p>
+        <ul role="list" aria-label="Список свят" className="space-y-4">
+          {selectedHoliday.map((holiday) => {
+            const link = parashatLinks[holiday.title];
 
-            {holiday.hebrew && (
-              <p lang="he" dir="rtl" className="text-muted-foreground text-md">
-                {holiday.hebrew}
-              </p>
-            )}
-          </div>
-        ))}
+            return (
+              <li
+                key={holiday.title}
+                className="border-b pb-2 last:border-b-0 last:pb-0"
+              >
+                {link ? (
+                  <h3 className="text-lg font-bold">
+                    <Link to={link} className="text-blue-600">
+                      {translateHolidayTitle(holiday.title)}
+                    </Link>
+                  </h3>
+                ) : (
+                  <h3 className="text-lg font-bold">
+                    {translateHolidayTitle(holiday.title)}
+                  </h3>
+                )}
+
+                {holiday.hebrew && (
+                  <p
+                    lang="he"
+                    dir="rtl"
+                    className="text-muted-foreground text-md"
+                  >
+                    {holiday.hebrew}
+                  </p>
+                )}
+              </li>
+            );
+          })}
+        </ul>
       </CardContent>
     </Card>
   );
