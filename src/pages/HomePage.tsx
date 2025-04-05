d2f63bc1d91fc366ebf94ed0f74ec06c26f1aea3
etopritika <dimaprytyka@gmail.com>
2025-04-05 21:31:54 +0300

    Updated home-page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+
+import HomepageVerseCard from "@/components/Homepage/HomepageVerseCard";
+import HomepageVerseList from "@/components/Homepage/HomepageVerseList";
+import { toast } from "@/hooks/use-toast";
+import { fetchHomepageVerses } from "@/lib/api/fetchFirestoreData";
+import { Verse } from "@/lib/types";
+
 export default function HomePage() {
+  const [verses, setVerses] = useState<Verse[]>([]);
+
+  useEffect(() => {
+    const fetchVerses = async () => {
+      try {
+        const data = await fetchHomepageVerses();
+        setVerses(data);
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : "Неизвестная ошибка";
+        toast({
+          title: "Ошибка",
+          description: errorMessage,
+          variant: "destructive",
+        });
+      }
+    };
+
+    fetchVerses();
+  }, []);
+
   return (
-    <section className="flex h-full items-center justify-center py-6">
-      <span>Выберете книгу</span>
+    <section className="flex h-full justify-center py-6">
+      <h1 className="sr-only">Главная страница</h1>
+      <HomepageVerseList>
+        {verses.map((verse) => (
+          <HomepageVerseCard
+            key={`${verse.id_book}-${verse.id_chapter}-${verse.id_chapter_two || 1}-${verse.poemNumber}`}
+            verse={verse}
+            setVerses={setVerses}
+          />
+        ))}
+      </HomepageVerseList>
     </section>
   );
 }
