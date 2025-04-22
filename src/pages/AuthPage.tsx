b6e759d0deb9ec911b72833bdd5915c214ae72cd
etopritika <dimaprytyka@gmail.com>
2025-04-22 13:31:11 +0300

    Removed accessible title

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -14,14 +14,7 @@ export default function AuthPage() {
   }, [token, navigate]);
 
   return (
-    <section
-      aria-labelledby="auth-page-title"
-      className="flex min-h-screen items-center justify-center py-6"
-    >
-      <h1 id="auth-page-title" className="sr-only">
-        Вход в аккаунт
-      </h1>
-
+    <section className="flex min-h-screen items-center justify-center py-6">
       <AuthForm />
     </section>
   );
